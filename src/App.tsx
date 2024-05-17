7deb8b7461c0b1c3085b5b6fcacaa7161f89d4e9
Vinicius <152424182+PeNeTrAz@users.noreply.github.com>
2024-05-17 18:03:50 -0300

    base, leftside

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import styles from './App.module.css'
+import poweredImage from './assets/powered.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [heightField, setHeightField] = useState(0)
+    const [weightField, setWeightField] = useState(0)
+
+    const handleCalculateButton = () => {
+        if (heightField && weightField) {
+
+        } else {
+            alert("Digite todos os campos!")
+        }
+    }
+
+    return (
+        <div className={styles.main}>
+            <header>
+                <div className={styles.headerContainer}>
+                    <img src={poweredImage} alt="Logo" width={100} />
+                </div>
+            </header>
+            <div className={styles.container}>
+                <div className={styles.leftSide}>
+                    <h1>Calcule o seu IMC.</h1>
+                    <input
+                        type="number"
+                        placeholder="Digite a sua altura. Ex: 1,80 (em metros)"
+                        value={heightField > 0 ? heightField : ''}
+                        onChange={e => setHeightField(parseFloat(e.target.value))}
+                    />
+                    <input
+                        type="number"
+                        placeholder="Digite o seu peso. Ex: 80 (em kg)"
+                        value={weightField > 0 ? weightField : ''}
+                        onChange={e => setWeightField(parseFloat(e.target.value))}
+                    />
+                    <button onClick={handleCalculateButton}>Calcular</button>
+                </div>
+                <div className={styles.rightSide}>
+                    ...
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
